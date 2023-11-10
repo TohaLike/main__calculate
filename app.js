4c9707ef8e7a400193604a1714a02268e6adc0a4
Anton <thetoxalike444@gmail.com>
2023-11-10 11:35:34 +0300

    i added new css button

--- app.js
+++ app.js
@@ -23,6 +23,38 @@ const calcBtnComma = document.querySelector('.calc__btn__comma')
 const calcBtnEqual = document.querySelector('.calc__btn__equal')
 
 
+// window.addEventListener('load', () => {
+//    return resultMatrix.innerHTML = 0
+// })
+
+function matrixResult() {
+   const a11 = parseFloat(inputOne.value)
+   const a12 = parseFloat(inputTwo.value)
+   const a13 = parseFloat(inputThree.value)
+   const a21 = parseFloat(inputFour.value)
+   const a22 = parseFloat(inputFive.value)
+   const a23 = parseFloat(inputSix.value)
+   const a31 = parseFloat(inputSeven.value)
+   const a32 = parseFloat(inputEight.value)
+   const a33 = parseFloat(inputNine.value)
+
+   let createResultBlock = document.createElement('div')
+   createResultBlock.textContent = `${a11 * a22 * a33} + ${a12 * a23 * a31} 
+   + ${a13 * a21 * a32} - ${a13 * a22 * a31} - ${a11 * a23 * a32} - ${a12 * a21 * a33} =
+   ${a11 * a22 * a33 + a12 * a23 * a31 + a13 * a21 * a32 - a13 * a22 * a31 - a11 * a23 * a32 - a12 * a21 * a33}`
+   createResultBlock.className = `result__matrix__main`
+   resultMatrix.innerHTML = ''
+   resultMatrix.appendChild(createResultBlock)
+
+   console.log(a11)
+}
+
+
+calcBtnEqual.addEventListener('mousedown', () => {
+   return matrixResult()
+})
+
+
 
 inputOne.addEventListener('mousedown', () => {
    keyBoard(inputOne)
@@ -93,60 +125,14 @@ function keyBoard(input) {
       input.value += 0;
    }
    calcBtnComma.onclick = () => {
-      input.value += ',';
+      input.value += '.'
+      input.value = input.value.replace(/\./g, ',')
    }
 }
 
-
-
-window.addEventListener('load', () => {
-   return resultMatrix.innerHTML = 0
-})
-
-function matrix() {
-   const a11 = parseInt(inputOne.value)
-   const a12 = parseInt(inputTwo.value)
-   const a13 = parseInt(inputThree.value)
-   const a21 = parseInt(inputFour.value)
-   const a22 = parseInt(inputFive.value)
-   const a23 = parseInt(inputSix.value)
-   const a31 = parseInt(inputSeven.value)
-   const a32 = parseInt(inputEight.value)
-   const a33 = parseInt(inputNine.value)
-
-   let result = a11 * a22 * a33 + a12 * a23 * a31 + a13 * a21 * a32 -
-      a13 * a22 * a31 - a11 * a23 * a32 - a12 * a21 * a33;
-
-   return result
+function replacePoint(inputPoint) {
+   return inputPoint.split(' ')
 }
-
-function matrixResult() {
-   const a11 = parseFloat(inputOne.value)
-   const a12 = parseFloat(inputTwo.value)
-   const a13 = parseFloat(inputThree.value)
-   const a21 = parseFloat(inputFour.value)
-   const a22 = parseFloat(inputFive.value)
-   const a23 = parseFloat(inputSix.value)
-   const a31 = parseFloat(inputSeven.value)
-   const a32 = parseFloat(inputEight.value)
-   const a33 = parseFloat(inputNine.value)
-
-   let createResultBlock = document.createElement('div')
-   createResultBlock.textContent = `${a11 * a22 * a33} + ${a12 * a23 * a31} 
-   + ${a13 * a21 * a32} - ${a13 * a22 * a31} - ${a11 * a23 * a32} - ${a12 * a21 * a33} = ${matrix()}`
-   createResultBlock.className = `result__matrix__main`
-   resultMatrix.innerHTML = ''
-   resultMatrix.appendChild(createResultBlock)
-}
-
-
-calcBtnEqual.addEventListener('mousedown', () => {
-   return matrixResult()
-})
-
-
-
-
 
 
 
